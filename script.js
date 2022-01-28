60251e1a6829de62bb5a0c980aef913d7d878e4d
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2022-01-28 23:36:03 +0800

    Add basic scoresheet

--- script.js
+++ script.js
@@ -1,130 +1,146 @@
-const QUESTIONS_COUNT = 3
-const OPTIONS_COUNT = 4
-const operators = ["-", "+", "*"]
-const questionsContainer = document.getElementById("questions")
-const questions = []
-const selection = []
-const results = []
+const QUESTIONS_COUNT = 3;
+const OPTIONS_COUNT = 4;
+const operators = ["-", "+", "*"];
+const questionsContainer = document.getElementById("questions");
+const questions = [];
+const selection = [];
+const results = [];
 
 class Question {
   constructor(leftOperand, operator, rightOperand) {
-    this.leftOperand = leftOperand
-    this.operator = operator
-    this.rightOperand = rightOperand
-    this.string = `${leftOperand} ${operator} ${rightOperand}`
-    this.answer = eval(this.string)
-    this.options = generateOptions(this.answer)
+    this.leftOperand = leftOperand;
+    this.operator = operator;
+    this.rightOperand = rightOperand;
+    this.string = `${leftOperand} ${operator} ${rightOperand}`;
+    this.answer = eval(this.string);
+    this.options = generateOptions(this.answer);
   }
 }
 
 function generateOptions(correctAnswer) {
-  const options = [correctAnswer]
+  const options = [correctAnswer];
   while (options.length < OPTIONS_COUNT) {
-    const newOption = correctAnswer + getRandomNumber(-10, 10)
+    const newOption = correctAnswer + getRandomNumber(-10, 10);
     if (options.includes(newOption)) {
-      continue
+      continue;
     }
-    options.push(newOption)
+    options.push(newOption);
   }
 
-  const swapWith = getRandomNumber(0, OPTIONS_COUNT - 1)
+  const swapWith = getRandomNumber(0, OPTIONS_COUNT - 1);
   if (swapWith != 0) {
-    options[0] = options[swapWith]
-    options[swapWith] = correctAnswer
+    options[0] = options[swapWith];
+    options[swapWith] = correctAnswer;
   }
 
-  return options
+  return options;
 }
 
 function getRandomNumber(lower, upper) {
-  const range = upper - lower + 1
-  return Math.floor(Math.random() * range + lower)
+  const range = upper - lower + 1;
+  return Math.floor(Math.random() * range + lower);
 }
 
 function generateQuestion() {
-  const leftOperand = getRandomNumber(1, 10)
-  const operator = operators[getRandomNumber(0, 2)]
-  const rightOperand = getRandomNumber(1, 10)
-  return new Question(leftOperand, operator, rightOperand)
+  const leftOperand = getRandomNumber(1, 10);
+  const operator = operators[getRandomNumber(0, 2)];
+  const rightOperand = getRandomNumber(1, 10);
+  return new Question(leftOperand, operator, rightOperand);
 }
 
 function attachQuestion(question, questionNumber) {
-  const title = document.createElement("h2")
-  title.appendChild(document.createTextNode(question.string))
-  title.setAttribute("class", "question--title")
-  questionsContainer.appendChild(title)
+  const title = document.createElement("h2");
+  title.appendChild(document.createTextNode(question.string));
+  title.setAttribute("class", "question--title");
+  questionsContainer.appendChild(title);
 
-  const optionsDiv = document.createElement("div")
-  optionsDiv.setAttribute("class", "question--options")
+  const optionsDiv = document.createElement("div");
+  optionsDiv.setAttribute("class", "question--options");
 
   for (let i = 0; i < OPTIONS_COUNT; i++) {
-    const id = getOptionId(questionNumber, i)
-    const button = document.createElement("button")
-    button.setAttribute("id", id)
-    button.setAttribute("type", "button")
-    button.setAttribute("class", `question--button question--button--${i}`)
-    button.appendChild(document.createTextNode(question.options[i]))
-    button.addEventListener("click", () => { onSelection(questionNumber, i) })
-    optionsDiv.appendChild(button)
+    const id = getOptionId(questionNumber, i);
+    const button = document.createElement("button");
+    button.setAttribute("id", id);
+    button.setAttribute("type", "button");
+    button.setAttribute("class", `question--button question--button--${i}`);
+    button.appendChild(document.createTextNode(question.options[i]));
+    button.addEventListener("click", () => {
+      onSelection(questionNumber, i);
+    });
+    optionsDiv.appendChild(button);
   }
-  questionsContainer.appendChild(optionsDiv)
+  questionsContainer.appendChild(optionsDiv);
 }
 
 function onSelection(questionNumber, optionNumber) {
-  selection[questionNumber] = optionNumber
+  selection[questionNumber] = optionNumber;
   for (let i = 0; i < OPTIONS_COUNT; i++) {
-    const id = getOptionId(questionNumber, i)
-    const button = document.getElementById(id)
+    const id = getOptionId(questionNumber, i);
+    const button = document.getElementById(id);
     i == optionNumber
       ? button.classList.add("question--button--selected")
-      : button.classList.remove("question--button--selected")
+      : button.classList.remove("question--button--selected");
   }
 
-  verifyCompletion()
+  verifyCompletion();
 }
 
 function verifyCompletion() {
-  let isCompleted = true
+  let isCompleted = true;
   for (let i = 0; i < QUESTIONS_COUNT; i++) {
     if (isNaN(selection[i])) {
-      isCompleted = false
-      break
+      isCompleted = false;
+      break;
     }
   }
   if (isCompleted) {
-    document.getElementById("submit").classList.add("submit--button--completed")
+    document
+      .getElementById("submit")
+      .classList.add("submit--button--completed");
   }
 
-  return isCompleted
+  return isCompleted;
 }
 
 function checkAnswers() {
-  const isCompleted = verifyCompletion()
+  const isCompleted = verifyCompletion();
   if (!isCompleted) {
-    return
+    return;
   }
 
-  let score = 0
   for (let i = 0; i < QUESTIONS_COUNT; i++) {
-    const question = questions[i]
-    const selectedOption = selection[i]
-    const selectedAnswer = question.options[selectedOption]
-    if (selectedAnswer == question.answer) {
-      score++
-      results.push(true)
-    } else {
-      results.push(false)
-    }
+    const question = questions[i];
+    const selectedOption = selection[i];
+    const selectedAnswer = question.options[selectedOption];
+    results[i] = selectedAnswer == question.answer;
   }
-  alert(`${score}`)
+  showScore();
+}
+
+function showScore() {
+  score = results.reduce((sum, isCorrect) => (isCorrect ? sum + 1 : sum), 0);
+  const scoreDisplay = document.getElementById("score");
+  const maxScoreDisplay = document.getElementById("maxScore");
+  const scoreSheet = document.getElementById("scoresheet");
+
+  scoreDisplay.innerText = score;
+  maxScoreDisplay.innerText = `/${QUESTIONS_COUNT}`;
+
+  document.getElementById("scoresheetContainer").style.display = "block";
+
+  scoreSheet.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
 }
 
 function getOptionId(questionNumber, optionNumber) {
-  return `question${questionNumber}_option${optionNumber}`
+  return `question${questionNumber}_option${optionNumber}`;
 }
 
 for (let i = 0; i < QUESTIONS_COUNT; i++) {
-  const newQuestion = generateQuestion()
-  questions.push(newQuestion)
-  attachQuestion(newQuestion, i)
+  const newQuestion = generateQuestion();
+  questions.push(newQuestion);
+  attachQuestion(newQuestion, i);
 }
